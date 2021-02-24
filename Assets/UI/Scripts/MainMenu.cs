596517da71b905bfcfa125eb06b1999dba7716ec
PaulaGuilherme <guilhermersp@outlook.com.br>
2021-02-23 22:40:28 -0300

    UI and Art

---
+++ Assets/UI/Scripts/MainMenu.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void ShowInfo (GameObject info)
+    {
+        if (info.activeSelf)
+            info.SetActive(false);
+
+        else
+            info.SetActive(true);
+
+
+    }
+
+
+    public void PlayGame ()
+
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+
+    }
+
+    public void QuitGame ()
+
+    {
+        Debug.Log("QUIT!");
+
+        Application.Quit();
+
+    }
+
+}
---
+++ Assets/UI/Scripts/PauseMenu.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject pauseMenuUI;
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (GameIsPaused)
+            {
+                Continue();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Continue ()
+    {
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause ()
+    {
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+    }
+
+    //public void OptionsMenu ()
+    //{
+        //Debug.Log("Loading options...");
+
+   // }
+
+    public void QuitMenu ()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+}
