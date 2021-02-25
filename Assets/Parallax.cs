ddeba48920a49afb4119a5b612d0760112f4f9aa
lzocolar <63549376+lzocolar@users.noreply.github.com>
2021-02-25 15:22:39 -0300

    Environmet art

---
+++ Assets/Parallax.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallax : MonoBehaviour
+{
+
+    public Camera cam;
+
+    public Transform subject;
+
+    public float parallaxSpeed;
+
+    Vector2 startPosition;
+
+    float startZ;
+
+    Vector2 travel => (Vector2)cam.transform.position - startPosition;
+
+    float distanceFromSubject => transform.position.z - subject.position.z;
+
+    float clippingPlane => (cam.transform.position.z + (distanceFromSubject > 0 ? cam.farClipPlane : cam.nearClipPlane));
+
+    float parallaxFactor => Mathf.Abs(distanceFromSubject) / clippingPlane;
+
+    Vector2 prallaxFactor;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startPosition = transform.position;
+        startZ = transform.position.z;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Vector2 newPos = startPosition + travel * parallaxSpeed;
+        transform.position = new Vector3(newPos.x, newPos.y, startZ);
+    }
+}
